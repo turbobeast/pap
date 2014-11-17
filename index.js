3e5bd489f89b587f0919f846215d101cffcaa703
george michael faust <georgemichaelfaust@gmail.com>
2014-11-16 23:39:59 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,166 @@
+var Pap = (function () {
+    var pap = {};
+
+
+    pap.smear = function (imageData, radius) {
+
+
+        var columns = imageData.width;
+        var rows = imageData.height;
+        var chunkWidth = radius * 2; //112;
+        var chunks = [];
+
+        var currentRow = 0;
+        var currentColumn = 0;
+
+        var currentPixel = 0;
+        var pixelRow = 0;
+
+
+        var totalPixels = imageData.data.length / 4;
+        var pixels = [];
+
+        for(var i = 0; i < imageData.data.length; i += 4) {
+            pixels.push({
+                r : imageData.data[i],
+                g : imageData.data[i + 1],
+                b : imageData.data[i + 2],
+                a : imageData.data[i + 3]
+            });
+        }
+
+
+        var c = 0;
+        var totalChunkColumns = imageData.width / chunkWidth;
+        var totalChunkRows = imageData.height / chunkWidth;
+
+
+        for(var j = 0; j < totalChunkRows; j += 1) {
+            chunks[j] = [];
+            for(var c = 0; c < totalChunkColumns; c += 1) {
+                chunks[j][c] = [];
+            }
+        }
+
+        for(var k = 0; k < imageData.data.length; k += 4) {
+            c += 1;
+
+            var pixel = {
+                r : imageData.data[k],
+                g : imageData.data[k + 1],
+                b : imageData.data[k + 2],
+                a : imageData.data[k + 3]
+            };
+
+
+            if((c % chunkWidth) === 0) {
+
+                currentColumn += 1;
+
+                if(currentColumn === totalChunkColumns) {
+
+                    currentColumn = 0;
+                    pixelRow += 1;
+
+                    if(pixelRow % chunkWidth === 0) {
+                        currentRow += 1;
+                        pixelRow = 0;
+                    }
+
+                }
+            }
+
+            chunks[currentRow][currentColumn].push(pixel);
+            if(currentColumn % 2 === 0) {
+                if(currentRow % 2 === 0) {
+                    imageData.data[k] = imageData.data[k] / 2;
+                    imageData.data[k + 1] = imageData.data[k + 1] / 2;
+                    imageData.data[k + 2] = imageData.data[k + 2] / 2;
+
+                }
+            }
+
+        }
+
+
+        for(var j = 0; j < chunks.length; j += 1) {
+
+            var roe = chunks[j];
+
+
+            for(var h = 0; h < roe.length; h += 1) {
+                var callem = roe[h];
+                var totalRed = 0;
+                var totalGreen = 0;
+                var totalBlue = 0;
+
+                var averageRed,
+                averageGreen,
+                averageBlue;
+
+                for(var q = 0; q < callem.length; q += 1) {
+                    totalRed += callem[q].r;
+                    totalGreen += callem[q].g;
+                    totalBlue += callem[q].b;
+                }
+
+
+                averageRed = totalRed / callem.length;
+                averageGreen = totalGreen / callem.length;
+                averageBlue = totalBlue / callem.length;
+
+                for(var q = 0; q < callem.length; q += 1) {
+                    callem[q].r = averageRed;
+                    callem[q].g = averageGreen;
+                    callem[q].b = averageBlue;
+                }
+
+            }
+        }
+
+        currentRow = 0;
+        currentColumn = 0;
+        c = 0;
+
+
+        for(var k = 0; k < imageData.data.length; k += 4) {
+
+
+            c += 1;
+
+            if((c % chunkWidth) === 0) {
+
+                currentColumn += 1;
+
+                if(currentColumn === totalChunkColumns) {
+
+                    currentColumn = 0;
+                    pixelRow += 1;
+
+                    if(pixelRow % chunkWidth === 0) {
+                        currentRow += 1;
+                        pixelRow = 0;
+                    }
+
+                }
+            }
+
+            var blur = chunks[currentRow][currentColumn][0];
+
+            imageData.data[k] = blur.r;
+            imageData.data[k + 1] = blur.g;
+            imageData.data[k + 2] = blur.b;
+
+
+        }
+
+        return imageData;
+
+    };
+
+    return pap;
+
+}());
+
+
+module.exports = Pap;
