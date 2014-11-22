db0431c484849a3de3a4134af404c98819e912d0
george michael faust <georgemichaelfaust@gmail.com>
2014-11-22 15:03:41 -0500

    clean up and lint

--- index.js
+++ index.js
@@ -5,62 +5,31 @@ var Pap = (function () {
     var pap = {};
 
     function powerofTwo (num) {
-        return !!( (num & (num - 1)) == 0 );
+        return !!( (num & (num - 1)) === 0 );
     }
 
+    function pushPixelsIntoChunks (pixelData, chunkSize, totalChunkColumns) {
 
-    pap.smear = function (imageData, radius) {
+        var chunks = [],
+        currentColumn = 0,
+        currentRow = 0,
+        pixelRow = 0,
+        pixel = {},
+        c = 0,
+        i = 0;
 
-
-        if(!powerofTwo(radius)) {
-            console.warn('pixel size should be power of two for best results');
-        }
-        
-        var newData = document.createElement('canvas').getContext('2d').getImageData(0,0,imageData.width, imageData.height);
-
-        newData.data.set(imageData.data);
-
-        var columns = imageData.width;
-        var rows = imageData.height;
-        var chunkWidth = radius; //112;
-        var chunks = [];
-
-        var currentRow = 0;
-        var currentColumn = 0;
-
-        var currentPixel = 0;
-        var pixelRow = 0;
-
-
-        var totalPixels = imageData.data.length / 4;
-        var pixels = [];
-
-        for(var i = 0; i < imageData.data.length; i += 4) {
-            pixels.push({
-                r : imageData.data[i],
-                g : imageData.data[i + 1],
-                b : imageData.data[i + 2],
-                a : imageData.data[i + 3]
-            });
-        }
-
-
-        var c = 0;
-        var totalChunkColumns = imageData.width / chunkWidth;
-        var totalChunkRows = imageData.height / chunkWidth;
-
-        for(var k = 0; k < imageData.data.length; k += 4) {
+        for(i = 0; i < pixelData.length; i += 4) {
             c += 1;
 
-            var pixel = {
-                r : imageData.data[k],
-                g : imageData.data[k + 1],
-                b : imageData.data[k + 2],
-                a : imageData.data[k + 3]
+            pixel = {
+                r : pixelData[i],
+                g : pixelData[i + 1],
+                b : pixelData[i + 2],
+                a : pixelData[i + 3]
             };
 
 
-            if((c % chunkWidth) === 0) {
+            if((c % chunkSize) === 0) {
 
                 currentColumn += 1;
 
@@ -69,7 +38,7 @@ var Pap = (function () {
                     currentColumn = 0;
                     pixelRow += 1;
 
-                    if(pixelRow % chunkWidth === 0) {
+                    if(pixelRow % chunkSize === 0) {
                         currentRow += 1;
                         pixelRow = 0;
                     }
@@ -90,34 +59,53 @@ var Pap = (function () {
 
         }
 
-
-        for(var j = 0; j < chunks.length; j += 1) {
-
-            var roe = chunks[j];
+        return chunks;
+    }
 
 
-            for(var h = 0; h < roe.length; h += 1) {
-                var callem = roe[h];
-                var totalRed = 0;
-                var totalGreen = 0;
-                var totalBlue = 0;
 
-                var averageRed,
-                averageGreen,
-                averageBlue;
+    function averageOutChunkColors (chunks) {
 
-                for(var q = 0; q < callem.length; q += 1) {
+        var i = 0,
+        q = 0,
+        h = 0,
+        roe,
+        callem,
+        totalRed = 0,
+        totalGreen = 0,
+        totalBlue = 0,
+        averageRed = 0,
+        averageGreen = 0,
+        averageBlue = 0;
+
+        for(i = 0; i < chunks.length; i += 1) {
+
+            roe = chunks[i];
+
+            for(h = 0; h < roe.length; h += 1) {
+
+                callem = roe[h];
+                totalRed = 0;
+                totalGreen = 0;
+                totalBlue = 0;
+
+                averageRed = 0;
+                averageGreen = 0;
+                averageBlue = 0;
+
+                for(q = 0; q < callem.length; q += 1) {
+
                     totalRed += callem[q].r;
                     totalGreen += callem[q].g;
                     totalBlue += callem[q].b;
-                }
 
+                }
 
                 averageRed = totalRed / callem.length;
                 averageGreen = totalGreen / callem.length;
                 averageBlue = totalBlue / callem.length;
 
-                for(var q = 0; q < callem.length; q += 1) {
+                for(q = 0; q < callem.length; q += 1) {
                     callem[q].r = averageRed;
                     callem[q].g = averageGreen;
                     callem[q].b = averageBlue;
@@ -126,17 +114,24 @@ var Pap = (function () {
             }
         }
 
-        currentRow = 0;
-        currentColumn = 0;
-        c = 0;
+        return chunks;
+    }
 
 
-        for(var k = 0; k < imageData.data.length; k += 4) {
+    function pushChunksIntoImageData (newData, chunks, chunkSize, totalChunkColumns) {
 
+        var i = 0,
+        c = 0,
+        currentRow = 0,
+        currentColumn = 0,
+        pixelRow = 0,
+        blur;
+
+        for(i = 0; i < newData.data.length; i += 4) {
 
             c += 1;
 
-            if((c % chunkWidth) === 0) {
+            if((c % chunkSize) === 0) {
 
                 currentColumn += 1;
 
@@ -145,7 +140,7 @@ var Pap = (function () {
                     currentColumn = 0;
                     pixelRow += 1;
 
-                    if(pixelRow % chunkWidth === 0) {
+                    if(pixelRow % chunkSize === 0) {
                         currentRow += 1;
                         pixelRow = 0;
                     }
@@ -153,13 +148,36 @@ var Pap = (function () {
                 }
             }
 
-            var blur = chunks[currentRow][currentColumn][0];
+            blur = chunks[currentRow][currentColumn][0];
 
-            newData.data[k] = blur.r;
-            newData.data[k + 1] = blur.g;
-            newData.data[k + 2] = blur.b;
+            newData.data[i] = blur.r;
+            newData.data[i + 1] = blur.g;
+            newData.data[i + 2] = blur.b;
 
         }
+
+        return newData;
+    }
+
+
+    pap.smear = function (imageData, size) {
+
+
+        if(!powerofTwo(size)) {
+            console.warn('pixel size should be power of two for best results');
+        }
+
+        var newData,
+        chunkSize = size,
+        chunks = [],
+        totalChunkColumns = imageData.width / chunkSize;
+
+        newData = document.createElement('canvas').getContext('2d').getImageData(0,0,imageData.width, imageData.height);
+        newData.data.set(imageData.data);
+
+        chunks = pushPixelsIntoChunks(imageData.data, chunkSize, totalChunkColumns);
+        chunks = averageOutChunkColors(chunks);
+        newData = pushChunksIntoImageData(newData, chunks, chunkSize, totalChunkColumns);
 
         return newData;
 
